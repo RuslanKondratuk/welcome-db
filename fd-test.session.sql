f48a46d8c315aa7f85e8017c8c82fb9009db2c37
ruslan <ruslan.kondratuk46@gmail.com>
2022-12-22 19:08:26 +0200

    first commit

---
+++ fd-test.session.sql
@@ -0,0 +1,16 @@
+CREATE TABLE books (
+    name varchar(300),
+    author varchar(300),
+    type varchar(150),
+    pages int,
+    year date,
+    publisher varchar(256)
+)
+
+CREATE TABLE users (
+    first_name varchar(100),
+    last_name varchar(100),
+    email varchar(200),
+    birthday date,
+    gender varchar(100)
+)
